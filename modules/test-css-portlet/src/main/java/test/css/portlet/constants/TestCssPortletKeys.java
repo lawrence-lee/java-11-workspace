2c47a5b74f258f108f172b8068c5bdfbb2a3062e
Lawrence Lee <lawrence.lee@liferay.com>
2021-02-04 15:40:28 -0600

    Init workspace with sample projects

---
+++ modules/test-css-portlet/src/main/java/test/css/portlet/constants/TestCssPortletKeys.java
@@ -0,0 +1,11 @@
+package test.css.portlet.constants;
+
+/**
+ * @author lawrence
+ */
+public class TestCssPortletKeys {
+
+	public static final String TESTCSS =
+		"test_css_portlet_TestCssPortlet";
+
+}
